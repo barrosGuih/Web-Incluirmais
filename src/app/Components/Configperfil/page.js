c8ae4fc4a06b512e28d7cddbd9498ad07fa0a7ab
Guilherme <Guilherme>
2024-08-20 11:31:58 -0300

    Atualização autchat 20/08

--- src/app/Components/Configperfil/page.js
+++ src/app/Components/Configperfil/page.js
@@ -1,17 +1,29 @@
 import Style from './page.module.css';
+import Perfil from '../../../../public/perfil.png'
+import Image from 'next/image';
 
 export default function ConfigPerfil() {
   return (
     <div className={Style.container}>
       <div className={Style.boxperfil}>
-    
-    
-    
-    <div className={Style.ajustes}>
-      <img></img>
-      <h1 className={Style.hello}>Bem Vindo Sicrano!</h1>
 
-    </div>
+        <div className={Style.ajustes}>
+          <Image
+            src={Perfil}
+            className={Style.img}
+          ></Image>
+          <h1 className={Style.hello}>Bem Vindo Sicrano!</h1>
+        </div>
+
+        <div className={Style.botoesDiv}>
+          <h1 className={Style.configname}>Configurações da conta:</h1>
+          <button className={Style.botaoconfig}>Editar nome de usuário</button>
+          <button className={Style.botaoconfig}>Editar tipo de usuário</button>
+          <button className={Style.botaoconfig}>Configurações de privacidade</button>
+          <button className={Style.botaoconfig}>Mudar de conta</button>
+          <button className={Style.botaoconfig1}>Excluir conta</button>
+        </div>
+
       </div>
     </div>
   );
--- src/app/Components/navbar/page.js
+++ src/app/Components/navbar/page.js
@@ -1,9 +1,23 @@
 import style from './page.module.css';
+import Mano from '../../../../public/perfil.png'
+import Image from 'next/image';
 
 
 export default function Home() {
     return (
       <div className={style.container}>
+        <div className={style.conteudo}>
+        <div className={style.ajustes}>
+          <Image className={style.imgPerfil}
+            src={Mano}
+            width={150}
+            height={150}
+          ></Image>
+          <h1 className={style.hello}>@Sicrano</h1>
+        </div>
+        <button className={style.configbutao}>Configurações</button>
+
+        </div>
       </div>
     );
   }
