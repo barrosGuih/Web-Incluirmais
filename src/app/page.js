9609f0dfa4a4bdb81a0e80efb72bda2b4be57b03
Guilherme <Guilherme>
2024-09-24 11:46:00 -0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import Home1 from './navbar/page'
 import ConfigPerfil from './configperfil/page';
-=======
-import Home1 from './Components/navbar/page'
-import ConfigPerfil from './Components/Configperfil/page';
->>>>>>> 647768493cc4abec0344113fc856d4f8200be853
 import Style from './page.module.css';
 
 export default function Home() {
