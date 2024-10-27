beed14c00c2bbae9d629b31f3e736628acb27956
barrosGuihl <guilherme.barros@academico.ifpb.edu.br>
2024-10-27 12:43:14 -0300

    adição da tela de register de apoiados e logo

--- src/app/incluirapoiado/page.js
+++ src/app/incluirapoiado/page.js
@@ -12,8 +12,37 @@ export default function HomeInicial() {
             <Image className={Style.Logo} src={Incluir}/>
           </div>
           <h1 className={Style.texto1}>Incluir Aluno Apoiado</h1>
+
+          <div className={Style.forms}>
+
+            <div className={Style.containerforms}>
+          <label className={Style.laforms}>Nome:</label>
+            <input className={Style.forms} type='text'></input>
+          </div>
+
+          <div className={Style.containerforms}>
+          <label className={Style.laforms}>Matrícula:</label>
+            <input className={Style.forms} type='text'></input>
+          </div>
+
+          <div className={Style.containerforms}>
+          <label className={Style.laforms}>Turma:</label>
+            <input className={Style.forms} type='text'></input>
+          </div>
+
+          <div className={Style.containerdescr}>
+          <label className={Style.descr}>Descrição</label>
+            <textarea className={Style.conteudodescri}></textarea>
+          </div>
+
+          <div className={Style.enviardiv}>
+          <input className={Style.enviar} type='submit' placeholder='Incluir Apoiado'></input>
+          </div>
+
+
         </div>
-  
+
+        </div>
       </div>
     </div>
   );
