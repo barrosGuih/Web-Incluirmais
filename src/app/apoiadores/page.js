15aa6dacac0dbca45fcd66ee3d130080c7654add
BarrosGuih <progamadorbarros@gmail.com>
2026-01-28 19:42:48 -0300

    Atualizacao -  estilizacao das paginas incluir apoiado e apoiador, funcionamento do sistema de busca, ajustes extras na estilizacao

--- src/app/apoiadores/page.js
+++ src/app/apoiadores/page.js
@@ -1,59 +1,100 @@
 'use client';
+
 import Style from './page.module.css';
-import Perfil from './imgs/profile1.png';
-import Voltar from './imgs/voltar.png';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
-export default function Apoiadores({ params }) {
-  const [alunos, setAlunos] = useState([]);
+import Voltar from './imgs/voltar.png';
 
+export default function Apoiadores() {
+  const [alunos, setAlunos] = useState([]);
+  const [alunosFiltrados, setAlunosFiltrados] = useState([]);
+  const [busca, setBusca] = useState('');
+  
   const fetchAlunos = async () => {
     const res = await fetch('/api/apoiador');
     const data = await res.json();
     setAlunos(data);
-  }
+    setAlunosFiltrados(data);
+  };
 
   useEffect(() => {
     fetchAlunos();
   }, []);
 
+  const handleSearch = (texto) => {
+    setBusca(texto);
+
+  const filtrados = alunos.filter(a => 
+    a.nome.toLowerCase().includes(texto.toLowerCase())
+  );
+
+  setAlunosFiltrados(filtrados);
+}
+
   return (
     <div className={Style.container}>
       <div className={Style.boxperfil_bground1}>
-      <Link href={'./homeInicial'} className={Style.voltarBack}><Image src={Voltar} className={Style.imgVoltar}></Image></Link>
-      <div className={Style.divSearch}>
 
-        <div className={Style.iconSearch}></div>
-            <input placeholder= "Pesquisar alunos..." className={Style.search}></input>
-            {/*<Link href={'./homeInicial'}><button className={Style.butterc}>Voltar ao inicio</button></Link>*/}
-            
-          </div>
-        <div className={Style.boxperfil}>
-        {/*tupla*/}
-          <div className={Style.tupla1}>
-            {alunos.map(a => 
-            <Link  href={"/perfilApoiador/" + a.id} className={Style.tuplas}> 
-            <div className={Style.tupla}>
-                <div className={Style.divdotexto}><div><h1 className={Style.nomee}> 
-                {a.nome}</h1> <h3 className={Style.info}>data nasc: {a.data_nasc}ㅤㅤㅤTurma: {a.turma}</h3> </div></div>
-                <Image className={Style.fotoperfil}
-                    src={a.foto} 
-                    width={75} 
-                    height={65}>
-                </Image>
-            </div>
-            </Link>
-            )}
-            </div>
-       
-        {/*tupla*/}
-            
+        {/* VOLTAR */}
+        <Link href="./homeInicial" className={Style.voltarBack}>
+          <Image src={Voltar} alt="Voltar" className={Style.imgVoltar} />
+        </Link>
+
+        {/* BUSCA */}
+        <div className={Style.divSearch}>
+          <div className={Style.iconSearch} />
+          <input
+            placeholder="Pesquisar alunos..."
+            value={busca}
+            onChange={(e)=> handleSearch(e.target.value)}
+            className={Style.search}
+          />
         </div>
+
+        {/* LISTA */}
+        <div className={Style.boxperfil}>
+          {alunosFiltrados.map(a => (
+            <Link
+              key={a.id}
+              href={`/perfilApoiador/${a.id}`}
+              className={Style.tuplas}
+            >
+              <div className={Style.tupla}>
+                <Image
+                  src={a.foto}
+                  width={64}
+                  height={64}
+                  alt={a.nome}
+                  className={Style.fotoperfil}
+                />
+
+                <div className={Style.divdotexto}>
+                  <span className={Style.nomee}>{a.nome}</span>
+                  <span className={Style.info}>
+                    Data nasc: {a.data_nasc}
+                  </span>
+                  <span className={Style.info}>
+                    Turma: {a.turma}
+                  </span>
+                </div>
+
+                <span className={Style.badgeStatus}>Ativo</span>
+              </div>
+            </Link>
+          ))}
+        </div>
+
+        {/* BOTÃO */}
         <div className={Style.botoessecun}>
-            <Link href={'./incluirapoiador'}><button className={Style.butterc}> INCLUIR NOVO APOIADOR</button></Link>
-          </div>
+          <Link href="./incluirapoiador">
+            <button className={Style.butterc}>
+              INCLUIR NOVO APOIADOR
+            </button>
+          </Link>
+        </div>
+
       </div>
     </div>
   );
--- src/app/apoiados/page.js
+++ src/app/apoiados/page.js
@@ -1,60 +1,101 @@
-'use client'
+'use client';
+
 import Style from './page.module.css';
-import Perfil from './imgs/profile1.png';
-import Voltar from './imgs/voltar.png';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
-export default function Apoiados ({ params }) {
-  const [alunos, setAlunos] = useState([]);
-  
-    const fetchAlunos = async () => {
-      const res = await fetch('/api/alunos');
-      const data = await res.json();
-      setAlunos(data);
-    }
-  
-    useEffect(() => {
-      fetchAlunos();
-    }, []);
-  
-    return (
-      <div className={Style.container}>
-        <div className={Style.boxperfil_bground1}>
-        <Link href={'./homeInicial'} className={Style.voltarBack}><Image src={Voltar} className={Style.imgVoltar}></Image></Link>
-        <div className={Style.divSearch}>
+import Voltar from './imgs/voltar.png';
 
-          <div className={Style.iconSearch}></div>
-              <input placeholder= "Pesquisar alunos..." className={Style.search}></input>
-              {/*<Link href={'./homeInicial'}><button className={Style.butterc}>Voltar ao inicio</button></Link>*/}
-              
-            </div>
-          <div className={Style.boxperfil}>
-          {/*tupla*/}
-            <div className={Style.tupla1}>
-              {alunos.map(a => 
-              <Link  href={"/perfilApoiado/" + a.id} className={Style.tuplas}> 
-              <div className={Style.tupla}>
-                  <div className={Style.divdotexto}><div><h1 className={Style.nomee}> 
-                  {a.nome}</h1> <h3 className={Style.info}>data nasc: {a.data_nasc}ㅤㅤㅤTurma: {a.turma}</h3> </div></div>
-                  <Image className={Style.fotoperfil}
-                      src={a.foto} 
-                      width={75} 
-                      height={65}>
-                  </Image>
-              </div>
-              </Link>
-              )}
-              </div>
-         
-          {/*tupla*/}
-              
-          </div>
-          <div className={Style.botoessecun}>
-              <Link href={'./incluirapoiado'}><button className={Style.butterc}> INCLUIR NOVO APOIADO</button></Link>
-            </div>
+export default function Apoiados() {
+  const [alunos, setAlunos] = useState([]);
+  const [alunosFiltrados, setAlunosFiltrados] = useState([]);
+  const [busca, setBusca] = useState('');
+
+  const fetchAlunos = async () => {
+    const res = await fetch('/api/alunos');
+    const data = await res.json();
+    setAlunos(data);
+    setAlunosFiltrados(data);
+  };
+
+  useEffect(() => {
+    fetchAlunos();
+  }, []);
+
+  const handleSearch = (texto) => {
+    setBusca(texto);
+
+  const filtrados = alunos.filter(a => 
+    a.nome.toLowerCase().includes(texto.toLowerCase())
+  );
+
+  setAlunosFiltrados(filtrados);
+}
+
+  return (
+    <div className={Style.container}>
+      <div className={Style.boxperfil_bground1}>
+
+        {/* VOLTAR */}
+        <Link href="./homeInicial" className={Style.voltarBack}>
+          <Image src={Voltar} alt="Voltar" className={Style.imgVoltar} />
+        </Link>
+
+        {/* BUSCA */}
+        <div className={Style.divSearch}>
+          <div className={Style.iconSearch} />
+          <input
+            placeholder="Pesquisar alunos..."
+            className={Style.search}
+            value={busca}
+            onChange={(e)=> handleSearch(e.target.value)}
+          />
         </div>
+
+        {/* LISTA */}
+        <div className={Style.boxperfil}>
+          {alunosFiltrados.map(a => (
+            <Link
+              key={a.id}
+              href={`/perfilApoiado/${a.id}`}
+              className={Style.tuplas}
+            >
+              <div className={Style.tupla}>
+                <Image
+                  src={a.foto}
+                  width={64}
+                  height={64}
+                  alt={a.nome}
+                  className={Style.fotoperfil}
+                />
+
+                <div className={Style.divdotexto}>
+                  <span className={Style.nomee}>{a.nome}</span>
+                  <span className={Style.info}>
+                    Data nasc: {a.data_nasc}
+                  </span>
+                  <span className={Style.info}>
+                    Turma: {a.turma}
+                  </span>
+                </div>
+
+                <span className={Style.badgeStatus}>Aluno</span>
+              </div>
+            </Link>
+          ))}
+        </div>
+
+        {/* BOTÃO */}
+        <div className={Style.botoessecun}>
+          <Link href="./incluirapoiado">
+            <button className={Style.butterc}>
+              INCLUIR NOVO APOIADO
+            </button>
+          </Link>
+        </div>
+
       </div>
-    );
+    </div>
+  );
 }
